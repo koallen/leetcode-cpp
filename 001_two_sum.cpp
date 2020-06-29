1679ebb8069e4ec24ebd8a2616d77bd7277aaef8
Siyuan Liu <me@shawnliu.me>
2020-06-29 23:21:53 +0800

    Create 001_two_sum.cpp

---
+++ 001_two_sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> returnVal(2, 0);
+        unordered_map<int, int> numToIndex;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            numToIndex[nums[i]] = i;
+        }
+        
+        int numNeeded;
+        for (int i = 0; i < nums.size(); ++i) {
+            numNeeded = target - nums[i];
+            auto it = numToIndex.find(numNeeded);
+            if (it != numToIndex.end() && it->second != i) {
+                returnVal[0] = i;
+                returnVal[1] = it->second;
+                return returnVal;
+            }
+        }
+        return returnVal;
+    }
+};
